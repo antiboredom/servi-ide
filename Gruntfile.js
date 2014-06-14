55db1bdc2ccbf75b1c7c0ff4b962386799f8b776
Sam <splavigne@gmail.com>
2014-06-14 19:06:20 -0400

    add icon and fonts

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,6 +4,7 @@ module.exports = function(grunt) {
     nodewebkit: {
       options: {
         build_dir: './dist',
+        mac_icns: './servi.icns'
         // choose what platforms to compile for here
         mac: true,
         win: false,
