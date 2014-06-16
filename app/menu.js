8c208d25ce54f65329844d6f55ccd2e1328a0530
Sam <splavigne@gmail.com>
2014-06-16 15:54:45 -0400

    add 'new window menu' and change 'save & run' to just 'run'

--- app/menu.js
+++ app/menu.js
@@ -1,33 +1,37 @@
 var menubar = new gui.Menu({ type: 'menubar' });
 var fileMenu = new gui.Menu();
 
-fileMenu.append(new gui.MenuItem({ label: 'New \t\t\u2318N', click: function(){
+fileMenu.append(new gui.MenuItem({ label: 'New File \t\t\u2318N', click: function(){
   editor.newFile();
 }}));
 
-fileMenu.append(new gui.MenuItem({ label: 'Open \t\t\u2318O', click: function(){
+fileMenu.append(new gui.MenuItem({ label: 'New Window \t\u21E7\u2318N', click: function(){
+  editor.newWindow();
+}}));
+
+fileMenu.append(new gui.MenuItem({ label: 'Open \t\t\t\u2318O', click: function(){
   editor.openProjectFolder();
 }}));
 
-fileMenu.append(new gui.MenuItem({ label: 'Close \t\t\u2318W', click: function(){
+fileMenu.append(new gui.MenuItem({ label: 'Close \t\t\t\u2318W', click: function(){
   editor.closeFile();
 }}));
 
-fileMenu.append(new gui.MenuItem({ label: 'Save \t\t\u2318S', click: function(){
+fileMenu.append(new gui.MenuItem({ label: 'Save \t\t\t\u2318S', click: function(){
   editor.saveFile();
 }}));
 
-fileMenu.append(new gui.MenuItem({ label: 'Save As \t\t\u21E7\u2318S', click: function(){
+fileMenu.append(new gui.MenuItem({ label: 'Save As \t\t\t\u21E7\u2318S', click: function(){
   editor.saveFileAs();
 }}));
 
 fileMenu.append(new gui.MenuItem({ type: 'separator' }));
 
-fileMenu.append(new gui.MenuItem({ label: 'Export \t\u2318E', click: function(){
+fileMenu.append(new gui.MenuItem({ label: 'Export \t\t\t\u2318E', click: function(){
   editor.export();
 }}));
 
-fileMenu.append(new gui.MenuItem({ label: 'Save & Run \t\u2318R', click: function(){
+fileMenu.append(new gui.MenuItem({ label: 'Run \t\t\t\u2318R', click: function(){
   editor.run();
 }}));
 
